ea539613d2690a7c37b1a4b879bc3befc524345c
ram verma <42866242+ramvrm5@users.noreply.github.com>
2023-03-15 10:21:57 +0530

    canges in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 //Components
 import { Header } from './components/Header';
+
 import { Balance } from "./components/Balance";
+
 import { IncomeExpense } from "./components/IncomeExpense";
+
 import { TransactionList } from "./components/TransactionList";
+
 import { AddTransaction } from "./components/AddTransaction";
 
 import { GlobalProvider } from './context/GlobalState';
